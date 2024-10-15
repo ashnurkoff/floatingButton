84cf114966a3f6f9305f08dd78acb83d4f24ed94
ashnurkoff <alex.shnurkoff@gmail.com>
2024-10-16 01:19:18 +0400

    Update floatingButton.js

--- floatingButton.js
+++ floatingButton.js
@@ -1,7 +1,4 @@
-// floatingButton.js
-
 (function ($) {
-    // Function to check if a specific element is in the viewport
     function isElementInView($element, offset = 0) {
         var elementHeight = $element.outerHeight(); 
         var scrollPos = $(window).scrollTop();
@@ -9,12 +6,11 @@
         return scrollPos < elementHeight - offset;
     }
 
-    // Function to initialize the floating button logic
     function initFloatingButton(options) {
         var $floatingButton = $(options.buttonSelector);
         var $triggerElement = $(options.triggerElementSelector);
         var offset = options.offset || 300;
-        var activeClass = options.activeClass || 'active'; // Default class is 'active'
+        var activeClass = options.activeClass || 'active';
 
         function toggleFloatingButton() {
             if (isElementInView($triggerElement, offset)) {
@@ -24,16 +20,13 @@
             }
         }
 
-        // Run on page load
         toggleFloatingButton();
 
-        // Scroll event listener
         $(window).on('scroll', function () {
             toggleFloatingButton();
         });
     }
 
-    // Expose the library function globally
     window.FloatingButton = {
         init: initFloatingButton
     };
